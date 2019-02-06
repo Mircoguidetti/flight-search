a7e30ae5d815fd6c258c1155f783ec26dd885d0b
github username <github email address>
2019-02-06 10:38:54 +0000

    Config heroku variables

--- server/config/keys/prod.js
+++ server/config/keys/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   'rapidApiKey': process.env.RAPID_API_KEY,
-  'sessionSecret': process.env.SESSION_SECRET
+  'mongoURI': process.env.MONGO_URI,
+  'cookieKey': process.env.COOKIE_KEY,
+  'googleClientID': process.env.GOOGLE_CLIENT_ID,
+  'googleClientSecret': process.env.GOOGLE_CLIENT_SECRET
 };
